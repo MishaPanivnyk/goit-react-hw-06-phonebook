4f44dce9e8bcea90dad75e8e9787d0c8804713be
Misha Panivnyk <panivnykm@gmail.com>
2023-01-09 15:22:41 +0200

    Update ContactLIst

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,27 +1,15 @@
-import PropTypes from 'prop-types';
-
+import { useSelector } from 'react-redux';
+import { getFilterList } from 'redux/selectors';
 import Contact from 'components/ContactList/Contact/Contact';
 import { ContactsList } from 'components/ContactList/ContactsList.styled';
 
-export default function ContactList({ contacts, handleDelete }) {
+export default function ContactList() {
+  const filterList = useSelector(getFilterList);
   return (
     <ContactsList>
-      {contacts.map(contact => (
-        <Contact
-          key={contact.id}
-          contact={contact}
-          handleDelete={handleDelete}
-        />
-      ))}
+      {filterList.map(({ id, name, number }) => {
+        return <Contact key={id} id={id} name={name} number={number} />;
+      })}
     </ContactsList>
   );
 }
-
-ContactsList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.exact({
-      id: PropTypes.string.isRequired,
-    }).isRequired
-  ),
-  handleDelete: PropTypes.func,
-};
