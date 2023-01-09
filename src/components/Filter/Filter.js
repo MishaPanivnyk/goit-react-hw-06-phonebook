579f8d761331b67ce5f8bfb26c16e48e8298a0ea
Misha Panivnyk <panivnykm@gmail.com>
2023-01-09 15:22:52 +0200

    Add Filter

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,25 +1,26 @@
-import PropTypes from 'prop-types';
 import { Container } from 'components/Filter/Filter.styled';
-import { nanoid } from 'nanoid';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter } from 'redux/filter/filterSlice';
+import { getFilter } from 'redux/filter/selectors';
+// import { getContacts } from 'redux/contacts/selectors';
 
-export default function Filter({ value, onChange }) {
-  const nameInputId = nanoid();
+export default function Filter() {
+  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
+  const handleOnChange = e => dispatch(setFilter(e.target.value));
+  // const contacts = useSelector(getContacts);
+
   return (
     <Container>
       <div>
-        <label htmlFor={nameInputId}>Find contacts by name</label>
+        <label htmlFor={filter}>Find contacts by name</label>
         <input
-          id={nameInputId}
           type="text"
           name="filter"
-          value={value}
-          onChange={onChange}
+          value={filter}
+          onChange={handleOnChange}
         />
       </div>
     </Container>
   );
 }
-Filter.propTypes = {
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-};
